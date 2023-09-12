751ebbdcd27666dfd878f7f5546fdceedd2c6ddd
pbjer <prenticebjerk@gmail.com>
2023-09-11 18:40:28 -0700

    add delmiter comments and godoc

--- delimiters.go
+++ delimiters.go
@@ -1,17 +1,23 @@
 package hedi
 
+// DefaultDelimiters defines the default Delimiters used in EDI files.
 var DefaultDelimiters = Delimiters{
 	Segment:    '~',
 	Element:    '*',
 	SubElement: '~',
 }
 
+// Delimiters contains the delimiters used for splitting segments, elements,
+// and sub-elements in EDI files.
 type Delimiters struct {
 	Segment    rune
 	Element    rune
 	SubElement rune
 }
 
+// splitter returns a bufio.SplitFunc function for use in bufio.Scanner.
+// The returned function splits the data into tokens separated by the given rune.
+// This is commonly used for parsing segments, elements, or sub-elements in EDI files.
 func splitter(separator rune) func(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
 		for i := 0; i < len(data); i++ {
--- go.mod
+++ go.mod
@@ -7,5 +7,9 @@ require github.com/stretchr/testify v1.8.4
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
