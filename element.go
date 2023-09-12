37ef08f2588b4b412543d66d87e437a0ce698b0a
pbjer <prenticebjerk@gmail.com>
2023-09-11 18:43:47 -0700

    add element comments

--- element.go
+++ element.go
@@ -4,17 +4,20 @@ import (
 	"strings"
 )
 
+// Element represents an individual EDI element, containing a value and optional sub-elements.
 type Element struct {
 	Value       string
 	SubElements []string
 }
 
-// String returns a string of the element.
+// String returns the default delimited string representation of the Element.
+// It uses the DefaultDelimiters for formatting.
 func (e *Element) String() string {
 	return e.DString(DefaultDelimiters)
 }
 
-// DString returns a string of the element with the provided delimiters.
+// DString returns a delimited string representation of the Element.
+// It formats the Element's value and sub-elements using the provided Delimiters.
 func (e *Element) DString(delimiters Delimiters) string {
 	var sb strings.Builder
 
@@ -28,7 +31,8 @@ func (e *Element) DString(delimiters Delimiters) string {
 	return sb.String()
 }
 
-// AddSubElement adds a sub-element at the end of the current segment.
+// AddSubElement appends a sub-element value to the Element's SubElements slice.
+// Initializes SubElements if it is nil.
 func (e *Element) AddSubElement(value string) {
 	if e.SubElements == nil {
 		e.SubElements = []string{}
@@ -36,9 +40,11 @@ func (e *Element) AddSubElement(value string) {
 	e.SubElements = append(e.SubElements, value)
 }
 
+// Elements is a slice of Element structs, often representing a list of elements in an EDI segment.
 type Elements []Element
 
-// Last returns the last element in an element list.
+// Last returns the last Element in the Elements slice.
+// Returns nil and false if the Elements slice is empty.
 func (ee *Elements) Last() (*Element, bool) {
 	if len(*ee) == 0 {
 		return nil, false
