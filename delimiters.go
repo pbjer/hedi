15469e579eabf9af419343a677789fa29a54854f
pbjer <prenticebjerk@gmail.com>
2023-09-11 19:14:05 -0700

    move splitter

--- delimiters.go
+++ delimiters.go
@@ -14,20 +14,3 @@ type Delimiters struct {
 	Element    rune
 	SubElement rune
 }
-
-// splitter returns a bufio.SplitFunc function for use in bufio.Scanner.
-// The returned function splits the data into tokens separated by the given rune.
-// This is commonly used for parsing segments, elements, or sub-elements in EDI files.
-func splitter(separator rune) func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		for i := 0; i < len(data); i++ {
-			if data[i] == byte(separator) {
-				return i + 1, data[:i], nil
-			}
-		}
-		if atEOF && len(data) != 0 {
-			return len(data), data, nil
-		}
-		return 0, nil, nil
-	}
-}
