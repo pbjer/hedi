2ffb93284f8ea079babcfea6806b6f70a95a034d
pbjer <prenticebjerk@gmail.com>
2023-09-11 19:13:16 -0700

    add token comments

--- token.go
+++ token.go
@@ -1,16 +1,30 @@
 package hedi
 
+// TokenType represents the type of token in the parser.
 type TokenType string
 
+// Enumerated TokenTypes for various elements in the segment structure.
 const (
-	SegmentIdentifier   TokenType = "segment_identifier"
-	SegmentTerminator   TokenType = "segment_terminator"
-	ElementValue        TokenType = "element_value"
-	ElementDelimiter    TokenType = "element_delimiter"
-	SubElementValue     TokenType = "sub_element_value"
+	// SegmentIdentifier represents the type of token that identifies a segment.
+	SegmentIdentifier TokenType = "segment_identifier"
+
+	// SegmentTerminator represents the type of token that terminates a segment.
+	SegmentTerminator TokenType = "segment_terminator"
+
+	// ElementValue represents the type of token that holds the value of an element.
+	ElementValue TokenType = "element_value"
+
+	// ElementDelimiter represents the type of token that delimits elements.
+	ElementDelimiter TokenType = "element_delimiter"
+
+	// SubElementValue represents the type of token that holds the value of a sub-element.
+	SubElementValue TokenType = "sub_element_value"
+
+	// SubElementDelimiter represents the type of token that delimits sub-elements.
 	SubElementDelimiter TokenType = "sub_element_delimiter"
 )
 
+// Token structure holding the type and value of a parsed token.
 type Token struct {
 	Type  TokenType `json:"type"`
 	Value string    `json:"value"`
